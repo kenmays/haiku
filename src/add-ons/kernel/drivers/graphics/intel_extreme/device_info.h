8fb01aa0f205aa3378c476c6a74051e5835c7d26
kenmays <maybird1776@yahoo.com>
2026-09-10 18:02:03 -0700

    intel_extreme: add device capability model

---
+++ src/add-ons/kernel/drivers/graphics/intel_extreme/device_info.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2026, Haiku, Inc. All Rights Reserved.
+ * Distributed under the terms of the MIT License.
+ */
+#ifndef INTEL_EXTREME_DEVICE_INFO_H
+#define INTEL_EXTREME_DEVICE_INFO_H
+
+#include <SupportDefs.h>
+
+#include "intel_extreme.h"
+
+
+enum intel_device_capability {
+	INTEL_CAP_LEGACY_DISPLAY = 1 << 0,
+	INTEL_CAP_DDI = 1 << 1,
+	INTEL_CAP_AUX = 1 << 2,
+	INTEL_CAP_HOTPLUG = 1 << 3,
+	INTEL_CAP_HW_CURSOR = 1 << 4,
+	INTEL_CAP_RING = 1 << 5,
+	INTEL_CAP_BLITTER = 1 << 6,
+	INTEL_CAP_PCH_DISPLAY = 1 << 7
+};
+
+
+struct intel_device_profile {
+	uint16 device_id;
+	DeviceType type;
+	uint8 generation;
+	uint8 pipe_count;
+	uint8 port_count;
+	uint32 capabilities;
+	const char* name;
+};
+
+
+intel_device_profile intel_get_device_profile(uint16 deviceID,
+	DeviceType type, const char* name);
+
+#endif

1942b5b1a82592590f52831adc2577c419de2231
kenmays <maybird1776@yahoo.com>
2026-09-10 19:59:37 -0700

    intel_extreme: derive device capabilities from generation metadata

--- src/add-ons/kernel/drivers/graphics/intel_extreme/device_info.cpp
+++ src/add-ons/kernel/drivers/graphics/intel_extreme/device_info.cpp
@@ -9,38 +9,41 @@ intel_device_profile
 intel_get_device_profile(uint16 deviceID, DeviceType type, const char* name)
 {
 	const uint8 generation = (uint8)type.Generation();
+	const intel_generation_info& generationInfo
+		= intel_get_generation_info(generation);
+
 	intel_device_profile profile = {
 		deviceID,
 		type,
 		generation,
-		2,
-		4,
+		generationInfo.pipe_count,
+		generationInfo.port_count,
 		0,
 		name
 	};
 
-	/* Pipe topology follows the display engine rather than the PCI ID list. */
-	if (generation >= 12) {
-		profile.pipe_count = 4;
-		profile.port_count = 7;
-	} else if (generation >= 7) {
-		profile.pipe_count = 3;
-		profile.port_count = 6;
-	}
-
-	if (generation < 8)
+	if (generationInfo.display_engine == INTEL_DISPLAY_ENGINE_LEGACY)
 		profile.capabilities |= INTEL_CAP_LEGACY_DISPLAY;
-	else
-		profile.capabilities |= INTEL_CAP_DDI | INTEL_CAP_AUX
-			| INTEL_CAP_HOTPLUG;
 
-	if (generation >= 6)
-		profile.capabilities |= INTEL_CAP_HW_CURSOR | INTEL_CAP_RING;
+	if (generationInfo.has_ddi)
+		profile.capabilities |= INTEL_CAP_DDI;
 
-	if (generation >= 8)
+	if (generationInfo.has_aux)
+		profile.capabilities |= INTEL_CAP_AUX;
+
+	if (generationInfo.has_hotplug)
+		profile.capabilities |= INTEL_CAP_HOTPLUG;
+
+	if (generationInfo.has_hw_cursor)
+		profile.capabilities |= INTEL_CAP_HW_CURSOR;
+
+	if (generationInfo.has_ring)
+		profile.capabilities |= INTEL_CAP_RING;
+
+	if (generationInfo.has_blitter)
 		profile.capabilities |= INTEL_CAP_BLITTER;
 
-	if (generation >= 5 && generation <= 7)
+	if (generationInfo.has_pch)
 		profile.capabilities |= INTEL_CAP_PCH_DISPLAY;
 
 	return profile;

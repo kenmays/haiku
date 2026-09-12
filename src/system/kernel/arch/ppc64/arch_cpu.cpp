8772860c25b854398850b85273a3eb669be1a40d
kenmays <maybird1776@yahoo.com>
2026-09-12 07:34:55 -0700

    ppc64: invoke Apple G5 platform bootstrap from CPU lifecycle

--- src/system/kernel/arch/ppc64/arch_cpu.cpp
+++ src/system/kernel/arch/ppc64/arch_cpu.cpp
@@ -7,14 +7,23 @@
 #include <vm/VMAddressSpace.h>
 #include <string.h>
 
+extern "C" status_t apple_g5_platform_init(kernel_args* args);
+extern "C" status_t apple_g5_platform_init_post_vm(kernel_args* args);
+
 status_t arch_cpu_preboot_init_percpu(kernel_args*, int)
 {
 	set_msr(get_msr() | MSR_FP_AVAILABLE | MSR_64BIT);
 	arch_thread_set_current_thread(NULL);
 	return B_OK;
 }
-status_t arch_cpu_init(kernel_args*) { return B_OK; }
-status_t arch_cpu_init_post_vm(kernel_args*) { return B_OK; }
+status_t arch_cpu_init(kernel_args* args)
+{
+	return apple_g5_platform_init(args);
+}
+status_t arch_cpu_init_post_vm(kernel_args* args)
+{
+	return apple_g5_platform_init_post_vm(args);
+}
 status_t arch_cpu_init_percpu(kernel_args*, int) { return B_OK; }
 status_t arch_cpu_init_post_modules(kernel_args*) { return B_OK; }
 
@@ -33,78 +42,46 @@ void arch_cpu_sync_icache(void* address, size_t length)
 }
 void arch_cpu_memory_read_barrier(void) { asm volatile("lwsync" ::: "memory"); }
 void arch_cpu_memory_write_barrier(void) { asm volatile("eieio" ::: "memory"); }
-
 void arch_cpu_invalidate_tlb_range(intptr_t, addr_t start, addr_t end)
 {
 	ppc_sync();
-	for (; start < end; start += B_PAGE_SIZE)
-		tlbie(start);
-	tlbsync();
-	ppc_sync();
+	for (; start < end; start += B_PAGE_SIZE) tlbie(start);
+	tlbsync(); ppc_sync();
 }
 void arch_cpu_invalidate_tlb_list(intptr_t, addr_t pages[], int count)
 {
 	ppc_sync();
-	for (int i = 0; i < count; i++)
-		tlbie(pages[i]);
-	tlbsync();
-	ppc_sync();
+	for (int i = 0; i < count; i++) tlbie(pages[i]);
+	tlbsync(); ppc_sync();
 }
 void arch_cpu_global_tlb_invalidate() { ppc64_slb_invalidate(); }
 void arch_cpu_user_tlb_invalidate(intptr_t) { arch_cpu_global_tlb_invalidate(); }
 
-status_t arch_cpu_user_memcpy(void* to, const void* from, size_t size,
-	addr_t* faultHandler)
+status_t arch_cpu_user_memcpy(void* to, const void* from, size_t size, addr_t* faultHandler)
 {
 	addr_t old = *faultHandler;
 	if (ppc_set_fault_handler(faultHandler, (addr_t)&&error)) goto error;
-	memcpy(to, from, size);
-	*faultHandler = old;
-	return B_OK;
-error:
-	*faultHandler = old;
-	return B_BAD_ADDRESS;
+	memcpy(to, from, size); *faultHandler = old; return B_OK;
+error: *faultHandler = old; return B_BAD_ADDRESS;
 }
 
-ssize_t arch_cpu_user_strlcpy(char* to, const char* from, size_t size,
-	addr_t* faultHandler)
+ssize_t arch_cpu_user_strlcpy(char* to, const char* from, size_t size, addr_t* faultHandler)
 {
 	addr_t old = *faultHandler;
 	if (ppc_set_fault_handler(faultHandler, (addr_t)&&error)) goto error;
 	ssize_t length = strlen(from);
-	if (size != 0) {
-		size_t n = length < (ssize_t)size - 1 ? length : size - 1;
-		memcpy(to, from, n);
-		to[n] = 0;
-	}
-	*faultHandler = old;
-	return length;
-error:
-	*faultHandler = old;
-	return B_BAD_ADDRESS;
+	if (size != 0) { size_t n = length < (ssize_t)size - 1 ? length : size - 1; memcpy(to, from, n); to[n] = 0; }
+	*faultHandler = old; return length;
+error: *faultHandler = old; return B_BAD_ADDRESS;
 }
 
-status_t arch_cpu_user_memset(void* address, char value, size_t count,
-	addr_t* faultHandler)
+status_t arch_cpu_user_memset(void* address, char value, size_t count, addr_t* faultHandler)
 {
 	addr_t old = *faultHandler;
 	if (ppc_set_fault_handler(faultHandler, (addr_t)&&error)) goto error;
-	memset(address, value, count);
-	*faultHandler = old;
-	return B_OK;
-error:
-	*faultHandler = old;
-	return B_BAD_ADDRESS;
+	memset(address, value, count); *faultHandler = old; return B_OK;
+error: *faultHandler = old; return B_BAD_ADDRESS;
 }
 
-status_t arch_cpu_shutdown(bool reboot)
-{
-	PPCPlatform::Default()->ShutDown(reboot);
-	return B_ERROR;
-}
-
-bool ppc_set_fault_handler(addr_t* location, addr_t handler)
-{
-	*location = handler;
-	return false;
-}
+status_t arch_cpu_shutdown(bool reboot) { PPCPlatform::Default()->ShutDown(reboot); return B_ERROR; }
+bool ppc_set_fault_handler(addr_t* location, addr_t handler) { *location = handler; return false; }

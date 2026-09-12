ce4e8a57b4925ffae93876bd47f23d1c92a783a2
kenmays <maybird1776@yahoo.com>
2026-09-12 07:29:34 -0700

    ppc64: add architecture-specific thread types

---
+++ headers/private/kernel/arch/ppc64/arch_thread_types.h
@@ -0,0 +1,25 @@
+/*
+ * PowerPC 64-bit architecture thread types.
+ * Distributed under the terms of the MIT License.
+ */
+#ifndef KERNEL_ARCH_PPC64_THREAD_TYPES_H
+#define KERNEL_ARCH_PPC64_THREAD_TYPES_H
+
+#define IFRAME_TRACE_DEPTH 4
+
+struct iframe;
+struct iframe_stack {
+	struct iframe* frames[IFRAME_TRACE_DEPTH];
+	int32 index;
+};
+
+struct arch_thread {
+	void* sp;
+	void* interrupt_stack;
+	struct iframe_stack iframes;
+};
+
+struct arch_team { char dummy; };
+struct arch_fork_arg { char dummy; };
+
+#endif

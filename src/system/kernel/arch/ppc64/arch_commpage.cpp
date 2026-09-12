fbed377dbda35826f1bef8afe40d051970c5c0d6
kenmays <maybird1776@yahoo.com>
2026-09-12 15:47:42 -0700

    ppc64: add userspace signal trampoline

--- src/system/kernel/arch/ppc64/arch_commpage.cpp
+++ src/system/kernel/arch/ppc64/arch_commpage.cpp
@@ -8,6 +8,23 @@
 
 extern "C" void arch_user_thread_exit();
 
+extern "C" void __attribute__((noreturn))
+arch_user_signal_handler(signal_frame_data* data)
+{
+	if (data->siginfo_handler) {
+		auto handler = (void (*)(int, siginfo_t*, void*, void*))data->handler;
+		handler(data->info.si_signo, &data->info, &data->context, data->user_data);
+	} else {
+		auto handler = (void (*)(int, void*, vregs*))data->handler;
+		handler(data->info.si_signo, data->user_data, &data->context.uc_mcontext);
+	}
+
+	register uint64 syscallNumber asm("r0") = SYSCALL_RESTORE_SIGNAL_FRAME;
+	register addr_t argument asm("r3") = (addr_t)data;
+	asm volatile("sc" : "+r"(syscallNumber), "+r"(argument) :: "r4", "r5", "r6", "r7", "r8", "r9", "r10", "r11", "r12", "memory");
+	__builtin_unreachable();
+}
+
 static void
 register_commpage_function(const char* functionName, int32 commpageIndex,
 	const char* symbolName, addr_t expectedAddress)
@@ -31,5 +48,8 @@ arch_commpage_init_post_cpus(void)
 	register_commpage_function("arch_user_thread_exit",
 		COMMPAGE_ENTRY_PPC64_THREAD_EXIT, "commpage_thread_exit",
 		(addr_t)&arch_user_thread_exit);
+	register_commpage_function("arch_user_signal_handler",
+		COMMPAGE_ENTRY_PPC64_SIGNAL_HANDLER, "commpage_signal_handler",
+		(addr_t)&arch_user_signal_handler);
 	return B_OK;
 }

3a6665ea1fee325b15c27fb76e7343d685ca2a4d
kenmays <maybird1776@yahoo.com>
2026-09-12 15:10:00 -0700

    ppc64: distinguish U3 and U4 DART interfaces

--- src/system/kernel/platform/apple_g5/g5_dart.h
+++ src/system/kernel/platform/apple_g5/g5_dart.h
@@ -1,18 +1,16 @@
-/* Apple U3/U3H DART (DMA address remapping table) support. */
+/* Apple U3/U3H/U4 DART (DMA address remapping table) support. */
 #ifndef _KERNEL_APPLE_G5_DART_H
 #define _KERNEL_APPLE_G5_DART_H
 
 #include <SupportDefs.h>
 
 namespace AppleG5DART {
-
-status_t Init(addr_t base, size_t size);
+status_t Init(addr_t base, size_t size, bool u4 = false);
 void Shutdown();
 status_t Map(addr_t physical, size_t size, addr_t* _dmaAddress);
 status_t Unmap(addr_t dmaAddress, size_t size);
 addr_t Base();
 size_t Size();
-
 }
 
 #endif

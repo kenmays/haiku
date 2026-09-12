8bd434829602da9323dde6871cc43a50764b35c5
kenmays <maybird1776@yahoo.com>
2026-09-12 14:57:00 -0700

    ppc64: correct SLB ESID and user access bits

--- headers/private/kernel/arch/ppc64/arch_mmu.h
+++ headers/private/kernel/arch/ppc64/arch_mmu.h
@@ -1,11 +1,9 @@
 /* PPC970 Book III-S hashed page-table MMU definitions. */
 #ifndef _KERNEL_ARCH_PPC64_MMU_H
 #define _KERNEL_ARCH_PPC64_MMU_H
-
 #include <SupportDefs.h>
 #include <boot/kernel_args.h>
 #include <arch/cpu.h>
-
 #define PPC64_PAGE_SHIFT 12
 #define PPC64_PAGE_SIZE (1ULL << PPC64_PAGE_SHIFT)
 #define PPC64_SEGMENT_SHIFT 28
@@ -17,16 +15,17 @@
 #define PPC64_SLB_VSID_KP (1ULL << 10)
 #define PPC64_SLB_VSID_N (1ULL << 9)
 #define PPC64_SLB_VSID_L (1ULL << 8)
+#define PPC64_SLB_VSID_C (1ULL << 7)
 #define PPC64_SLB_VSID_SSIZE_SHIFT 62
 #define PPC64_SLB_SSIZE_256M 0
+#define PPC64_SLB_VSID_KERNEL PPC64_SLB_VSID_KP
+#define PPC64_SLB_VSID_USER (PPC64_SLB_VSID_KP | PPC64_SLB_VSID_KS | PPC64_SLB_VSID_C)
 #define PPC64_SLB_KERNEL_SLOT 0
 #define PPC64_SLB_USER_SLOT_BASE 8
 #define PPC64_SLB_USER_SLOT_COUNT 56
-
 #define PPC64_HPT_PTES_PER_GROUP 8
 #define PPC64_HPT_PTE_SIZE 16
 #define PPC64_HPT_PTEG_SIZE 128
-
 #define PPC64_HPTE_V_VALID 0x0000000000000001ULL
 #define PPC64_HPTE_V_SECONDARY 0x0000000000000002ULL
 #define PPC64_HPTE_V_LARGE 0x0000000000000004ULL
@@ -34,7 +33,6 @@
 #define PPC64_HPTE_V_H 0x4000000000000000ULL
 #define PPC64_HPTE_V_AVPN_MASK 0x3fffffffffffff80ULL
 #define PPC64_HPTE_V_SSIZE_SHIFT 62
-
 #define PPC64_HPTE_R_RPN 0x0ffffffffffff000ULL
 #define PPC64_HPTE_R_PP 0x0000000000000003ULL
 #define PPC64_HPTE_R_N 0x0000000000000004ULL
@@ -45,15 +43,12 @@
 #define PPC64_HPTE_R_C 0x0000000000000080ULL
 #define PPC64_HPTE_R_R 0x0000000000000100ULL
 #define PPC64_HPTE_R_WIMG 0x0000000000000078ULL
-
 #define PPC64_HPTE_PP_RWXX 0
 #define PPC64_HPTE_PP_RWRX 1
 #define PPC64_HPTE_PP_RWRW 2
 #define PPC64_HPTE_PP_RXRX 3
-
 struct ppc64_pte { uint64 word0; uint64 word1; };
 struct ppc64_pteg { ppc64_pte pte[PPC64_HPT_PTES_PER_GROUP]; };
-
 status_t ppc64_mmu_init(kernel_args* args);
 status_t ppc64_mmu_init_post_vm(kernel_args* args);
 void ppc64_mmu_switch_address_space(addr_t addressSpace);
@@ -68,5 +63,4 @@ status_t ppc64_query_page_asid(addr_t virtualAddress, phys_addr_t* physicalAddre
 	uint32* protection, addr_t addressSpace);
 status_t ppc64_clear_page_flags_asid(addr_t virtualAddress, uint32 flags,
 	bool* modified, addr_t addressSpace);
-
 #endif

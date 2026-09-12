46c104ab7db6fa272edcd00a56faba9dcbc7c132
kenmays <maybird1776@yahoo.com>
2026-09-12 07:33:20 -0700

    ppc64: expose Open Firmware platform interface

---
+++ headers/private/kernel/arch/ppc64/arch_platform.h
@@ -0,0 +1,5 @@
+/* PPC64 reuses the Open Firmware platform service interface. */
+#ifndef _KERNEL_PPC64_ARCH_PLATFORM_H
+#define _KERNEL_PPC64_ARCH_PLATFORM_H
+#include <arch/ppc/arch_platform.h>
+#endif

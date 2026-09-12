14c469c6e0afc7a12732d8e01048ad9a20531789
kenmays <maybird1776@yahoo.com>
2026-09-12 07:34:01 -0700

    ppc64: reserve SPRG1 for current-thread pointer

--- headers/private/kernel/arch/ppc64/arch_thread.h
+++ headers/private/kernel/arch/ppc64/arch_thread.h
@@ -13,16 +13,16 @@ void ppc_push_iframe(struct iframe_stack* stack, struct iframe* frame);
 void ppc_pop_iframe(struct iframe_stack* stack);
 struct iframe* ppc_get_user_iframe(void);
 
+/* SPRG1 is the current-thread pointer; SPRG2/3 are exception scratch. */
 static inline Thread* arch_thread_get_current_thread(void)
 {
 	Thread* thread;
-	asm volatile("mfsprg2 %0" : "=r"(thread));
+	asm volatile("mfsprg1 %0" : "=r"(thread));
 	return thread;
 }
-
 static inline void arch_thread_set_current_thread(Thread* thread)
 {
-	asm volatile("mtsprg2 %0" :: "r"(thread));
+	asm volatile("mtsprg1 %0" :: "r"(thread));
 }
 
 #ifdef __cplusplus

22bec851eda5ccac4208387645a98db870813d99
kenmays <maybird1776@yahoo.com>
2026-09-12 07:30:04 -0700

    ppc64: add native thread interface

---
+++ headers/private/kernel/arch/ppc64/arch_thread.h
@@ -0,0 +1,32 @@
+/* PowerPC 64-bit thread helpers. */
+#ifndef _KERNEL_ARCH_PPC64_THREAD_H
+#define _KERNEL_ARCH_PPC64_THREAD_H
+
+#include <arch/cpu.h>
+#include <thread.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void ppc_push_iframe(struct iframe_stack* stack, struct iframe* frame);
+void ppc_pop_iframe(struct iframe_stack* stack);
+struct iframe* ppc_get_user_iframe(void);
+
+static inline Thread* arch_thread_get_current_thread(void)
+{
+	Thread* thread;
+	asm volatile("mfsprg2 %0" : "=r"(thread));
+	return thread;
+}
+
+static inline void arch_thread_set_current_thread(Thread* thread)
+{
+	asm volatile("mtsprg2 %0" :: "r"(thread));
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif

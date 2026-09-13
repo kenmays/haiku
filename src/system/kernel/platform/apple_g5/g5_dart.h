b7384d931c50beccf21ccd59fc652696aa7f11bb
kenmays <maybird1776@yahoo.com>
2026-09-12 20:20:51 -0700

    apple_g5: expose DART physical DMA mapping

--- src/system/kernel/platform/apple_g5/g5_dart.h
+++ src/system/kernel/platform/apple_g5/g5_dart.h
@@ -7,8 +7,18 @@
 namespace AppleG5DART {
 status_t Init(addr_t base, size_t size, bool u4 = false);
 void Shutdown();
+
+/* Map physical memory into the 32-bit PCI DMA aperture. */
 status_t Map(addr_t physical, size_t size, addr_t* _dmaAddress);
 status_t Unmap(addr_t dmaAddress, size_t size);
+
+/* Explicit name for callers which are translating a physical segment. */
+inline status_t
+MapPhysical(addr_t physical, size_t size, addr_t* _dmaAddress)
+{
+	return Map(physical, size, _dmaAddress);
+}
+
 addr_t Base();
 size_t Size();
 }

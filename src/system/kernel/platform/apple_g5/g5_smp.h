4a70ece58d3e72826109e5c686bc928560b3ec49
kenmays <maybird1776@yahoo.com>
2026-09-12 21:11:42 -0700

    ppc64: expose G5 Open Firmware CPU start helper

--- src/system/kernel/platform/apple_g5/g5_smp.h
+++ src/system/kernel/platform/apple_g5/g5_smp.h
@@ -9,6 +9,11 @@ namespace AppleG5 {
 status_t smp_release_secondary(kernel_args* args, int32 cpu,
 	addr_t entry, addr_t stack);
 
+/* Start an OF-stopped CPU at a real-mode trampoline. physicalArgument is
+ * passed to the trampoline in PPC64 r3. */
+status_t smp_start_cpu_of(int32 cpu, addr_t realModePC,
+	addr_t physicalArgument);
+
 }
 
 #endif

43f0204feb5be0e8c3cb70259a9f74cae144ae8d
kenmays <maybird1776@yahoo.com>
2026-09-15 05:30:59 -0700

    graphics: add RDNA4 GFX12 command interface

---
+++ src/add-ons/kernel/drivers/graphics/amdgpu_rdna4/rdna4_gfx.h
@@ -0,0 +1,24 @@
+#ifndef RDNA4_GFX_H
+#define RDNA4_GFX_H
+
+#include <SupportDefs.h>
+
+struct rdna4_fence {
+	uint64 sequence;
+};
+
+class RDNA4GFX {
+public:
+	RDNA4GFX();
+	status_t Initialize();
+	status_t Submit(const uint32* commands, uint32 count,
+		rdna4_fence* _fence);
+	status_t Wait(const rdna4_fence& fence, bigtime_t timeout);
+	status_t Reset();
+
+private:
+	bool fInitialized;
+	uint64 fSequence;
+};
+
+#endif

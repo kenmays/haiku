ca65c4af34ce1a8e3b333623cd34db007a7316aa
kenmays <maybird1776@yahoo.com>
2026-09-15 06:03:19 -0700

    amdgpu_rdna4: complete baseline accelerant mode ABI

--- src/add-ons/accelerants/amdgpu_rdna4/accelerant_protos.h
+++ src/add-ons/accelerants/amdgpu_rdna4/accelerant_protos.h
@@ -9,8 +9,13 @@ void amdgpu_rdna4_uninit_accelerant();
 status_t amdgpu_rdna4_get_device_info(accelerant_device_info* info);
 uint32 amdgpu_rdna4_mode_count();
 status_t amdgpu_rdna4_get_mode_list(display_mode* modes);
+status_t amdgpu_rdna4_get_preferred_mode(display_mode* mode);
 status_t amdgpu_rdna4_get_display_mode(display_mode* mode);
 status_t amdgpu_rdna4_set_display_mode(display_mode* mode);
+status_t amdgpu_rdna4_get_frame_buffer_config(frame_buffer_config* config);
+status_t amdgpu_rdna4_get_pixel_clock_limits(display_mode* mode, uint32* low,
+	uint32* high);
+status_t amdgpu_rdna4_get_edid_info(void* info, size_t size, uint32* version);
 uint32 amdgpu_rdna4_dpms_capabilities();
 uint32 amdgpu_rdna4_dpms_mode();
 status_t amdgpu_rdna4_set_dpms_mode(uint32 mode);

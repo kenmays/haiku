4872c48fd42e958e9b7e74cd8c17cd1bd0dd20b2
kenmays <maybird1776@yahoo.com>
2026-09-15 05:50:19 -0700

    amdgpu_rdna4: wire complete accelerant hook table

--- src/add-ons/accelerants/amdgpu_rdna4/hooks.cpp
+++ src/add-ons/accelerants/amdgpu_rdna4/hooks.cpp
@@ -12,20 +12,20 @@ get_accelerant_hook(uint32 feature, void* data)
 			return (void*)amdgpu_rdna4_uninit_accelerant;
 		case B_GET_ACCELERANT_DEVICE_INFO:
 			return (void*)amdgpu_rdna4_get_device_info;
-		case B_ACCELERANT_MODE_COUNT:
-			return (void*)amdgpu_rdna4_mode_count;
-		case B_GET_MODE_LIST:
-			return (void*)amdgpu_rdna4_get_mode_list;
-		case B_GET_DISPLAY_MODE:
-			return (void*)amdgpu_rdna4_get_display_mode;
-		case B_SET_DISPLAY_MODE:
-			return (void*)amdgpu_rdna4_set_display_mode;
 		case B_DPMS_CAPABILITIES:
 			return (void*)amdgpu_rdna4_dpms_capabilities;
 		case B_DPMS_MODE:
 			return (void*)amdgpu_rdna4_dpms_mode;
 		case B_SET_DPMS_MODE:
 			return (void*)amdgpu_rdna4_set_dpms_mode;
+		case B_ACCELERANT_MODE_COUNT:
+			return (void*)amdgpu_rdna4_mode_count;
+		case B_GET_MODE_LIST:
+			return (void*)amdgpu_rdna4_get_mode_list;
+		case B_SET_DISPLAY_MODE:
+			return (void*)amdgpu_rdna4_set_display_mode;
+		case B_GET_DISPLAY_MODE:
+			return (void*)amdgpu_rdna4_get_display_mode;
 	}
 	return NULL;
 }

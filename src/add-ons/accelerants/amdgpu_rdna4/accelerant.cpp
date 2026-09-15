acc9f6e50913a8aab2e1f97c4194754243b5a8db
kenmays <maybird1776@yahoo.com>
2026-09-15 06:03:34 -0700

    amdgpu_rdna4: implement framebuffer and mode query hooks

--- src/add-ons/accelerants/amdgpu_rdna4/accelerant.cpp
+++ src/add-ons/accelerants/amdgpu_rdna4/accelerant.cpp
@@ -26,7 +26,6 @@ amdgpu_rdna4_init_accelerant(int fd)
 	data.shared_info_area = -1;
 	if (ioctl(fd, RDNA4_GET_PRIVATE_DATA, &data, sizeof(data)) < 0)
 		return B_ERROR;
-
 	if (data.shared_info_area < 0)
 		return B_ERROR;
 
@@ -40,7 +39,6 @@ amdgpu_rdna4_init_accelerant(int fd)
 	gSharedInfo = sShared;
 	memset(&sMode, 0, sizeof(sMode));
 
-	/* Boot-framebuffer mode is exposed when firmware supplied valid data. */
 	if (sShared->framebuffer_width != 0 && sShared->framebuffer_height != 0
 		&& sShared->framebuffer_pitch != 0) {
 		sMode.virtual_width = sShared->framebuffer_width;
@@ -98,10 +96,23 @@ amdgpu_rdna4_get_mode_list(display_mode* modes)
 }
 
 status_t
+amdgpu_rdna4_get_preferred_mode(display_mode* mode)
+{
+	if (!sInitialized || mode == NULL)
+		return B_BAD_VALUE;
+	if (sModeCount == 0)
+		return B_ENTRY_NOT_FOUND;
+	*mode = sMode;
+	return B_OK;
+}
+
+status_t
 amdgpu_rdna4_get_display_mode(display_mode* mode)
 {
 	if (!sInitialized || mode == NULL)
 		return B_BAD_VALUE;
+	if (sModeCount == 0)
+		return B_ENTRY_NOT_FOUND;
 	*mode = sMode;
 	return B_OK;
 }
@@ -116,6 +127,50 @@ amdgpu_rdna4_set_display_mode(display_mode* mode)
 		|| mode->space != sMode.space)
 		return B_NOT_SUPPORTED;
 	return B_OK;
+}
+
+status_t
+amdgpu_rdna4_get_frame_buffer_config(frame_buffer_config* config)
+{
+	if (!sInitialized || config == NULL)
+		return B_BAD_VALUE;
+	if (sShared->framebuffer_physical == 0 || sShared->framebuffer_pitch == 0)
+		return B_ENTRY_NOT_FOUND;
+
+	memset(config, 0, sizeof(*config));
+	config->frame_buffer = (void*)(addr_t)sShared->framebuffer_physical;
+	config->bytes_per_row = sShared->framebuffer_pitch;
+	return B_OK;
+}
+
+status_t
+amdgpu_rdna4_get_pixel_clock_limits(display_mode* mode, uint32* low, uint32* high)
+{
+	if (!sInitialized || mode == NULL || low == NULL || high == NULL)
+		return B_BAD_VALUE;
+	if (mode->timing.h_total == 0 || mode->timing.v_total == 0)
+		return B_BAD_VALUE;
+
+	uint64 pixels = (uint64)mode->timing.h_total * mode->timing.v_total;
+	uint64 minClock = pixels * 48 / 1000;
+	uint64 maxClock = pixels * 240 / 1000;
+	if (minClock > 0xffffffffULL || maxClock > 0xffffffffULL)
+		return B_BAD_VALUE;
+	*low = (uint32)minClock;
+	*high = (uint32)maxClock;
+	return B_OK;
+}
+
+status_t
+amdgpu_rdna4_get_edid_info(void* info, size_t size, uint32* version)
+{
+	(void)info;
+	(void)size;
+	(void)version;
+	if (!sInitialized)
+		return B_NO_INIT;
+	/* EDID/DDC/AUX is implemented by the future DCN4 connector layer. */
+	return B_NOT_SUPPORTED;
 }
 
 uint32
@@ -138,6 +193,5 @@ amdgpu_rdna4_set_dpms_mode(uint32 mode)
 	if (mode != B_DPMS_ON && mode != B_DPMS_STAND_BY
 		&& mode != B_DPMS_SUSPEND && mode != B_DPMS_OFF)
 		return B_BAD_VALUE;
-	/* Hardware DPMS is intentionally deferred until DCN4 ownership. */
 	return mode == B_DPMS_ON ? B_OK : B_NOT_SUPPORTED;
 }

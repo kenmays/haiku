050bc82a1192a9e33ec704432722611a024022aa
kenmays <maybird1776@yahoo.com>
2026-09-15 05:46:29 -0700

    amdgpu_rdna4: wire implemented accelerant hooks

--- src/add-ons/accelerants/amdgpu_rdna4/hooks.cpp
+++ src/add-ons/accelerants/amdgpu_rdna4/hooks.cpp
@@ -1,33 +1,31 @@
 #include "accelerant.h"
+#include "accelerant_protos.h"
 
-#include <Accelerant.h>
-
-extern "C" void* get_accelerant_hook(uint32 feature, void* data)
+extern "C" void*
+get_accelerant_hook(uint32 feature, void* data)
 {
 	(void)data;
-
 	switch (feature) {
 		case B_INIT_ACCELERANT:
-			return NULL;
+			return (void*)amdgpu_rdna4_init_accelerant;
 		case B_UNINIT_ACCELERANT:
-			return NULL;
+			return (void*)amdgpu_rdna4_uninit_accelerant;
 		case B_GET_ACCELERANT_DEVICE_INFO:
-			return NULL;
+			return (void*)amdgpu_rdna4_get_device_info;
 		case B_ACCELERANT_MODE_COUNT:
-			return NULL;
+			return (void*)amdgpu_rdna4_mode_count;
 		case B_GET_MODE_LIST:
-			return NULL;
-		case B_SET_DISPLAY_MODE:
-			return NULL;
+			return (void*)amdgpu_rdna4_get_mode_list;
 		case B_GET_DISPLAY_MODE:
-			return NULL;
+			return (void*)amdgpu_rdna4_get_display_mode;
+		case B_SET_DISPLAY_MODE:
+			return (void*)amdgpu_rdna4_set_display_mode;
 		case B_DPMS_CAPABILITIES:
-			return NULL;
+			return (void*)amdgpu_rdna4_dpms_capabilities;
 		case B_DPMS_MODE:
-			return NULL;
+			return (void*)amdgpu_rdna4_dpms_mode;
 		case B_SET_DPMS_MODE:
-			return NULL;
+			return (void*)amdgpu_rdna4_set_dpms_mode;
 	}
-
 	return NULL;
 }

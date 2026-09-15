cd27411ec8cdd58b1bfbe6df75011a90935fa8dd
kenmays <maybird1776@yahoo.com>
2026-09-15 05:57:24 -0700

    amdgpu_rdna4: fix multi-GPU lifecycle and probe path

--- src/add-ons/kernel/drivers/graphics/amdgpu_rdna4/driver.cpp
+++ src/add-ons/kernel/drivers/graphics/amdgpu_rdna4/driver.cpp
@@ -9,13 +9,11 @@
 #include <stdio.h>
 #include <string.h>
 
-#define MAX_CARDS 4
-
 static pci_module_info* sPCI;
-static char* sDeviceNames[MAX_CARDS + 1];
-static pci_info sPCIInfo[MAX_CARDS];
-static rdna4_shared_info sSharedInfo[MAX_CARDS];
-static area_id sSharedAreas[MAX_CARDS] = {-1, -1, -1, -1};
+static char* sDeviceNames[RDNA4_MAX_CARDS + 1];
+static pci_info sPCIInfo[RDNA4_MAX_CARDS];
+static rdna4_shared_info sSharedInfo[RDNA4_MAX_CARDS];
+static area_id sSharedAreas[RDNA4_MAX_CARDS];
 static int32 sDeviceCount;
 
 static status_t device_open(const char* name, uint32 flags, void** cookie);
@@ -28,14 +26,32 @@ static device_hooks sHooks = {
 	NULL, NULL, NULL, NULL, NULL, NULL
 };
 
+static void
+reset_devices()
+{
+	for (int32 i = 0; i < RDNA4_MAX_CARDS; i++) {
+		if (sSharedAreas[i] >= 0)
+			delete_area(sSharedAreas[i]);
+		sSharedAreas[i] = -1;
+		rdna4_device_uninit(sSharedInfo[i]);
+		free(sDeviceNames[i]);
+		sDeviceNames[i] = NULL;
+	}
+	sDeviceCount = 0;
+	sDeviceNames[0] = NULL;
+}
+
 static status_t
 find_devices()
 {
 	int32 cookie = 0;
 	sDeviceCount = 0;
-	pci_info info;
 
-	while (sDeviceCount < MAX_CARDS
+	for (int32 i = 0; i < RDNA4_MAX_CARDS; i++)
+		sSharedAreas[i] = -1;
+
+	pci_info info;
+	while (sDeviceCount < RDNA4_MAX_CARDS
 		&& sPCI->get_nth_pci_info(cookie++, &info) == B_OK) {
 		if (info.vendor_id != RDNA4_VENDOR_ID
 			|| info.class_base != PCI_display
@@ -46,7 +62,8 @@ find_devices()
 
 		int32 index = sDeviceCount;
 		sPCIInfo[index] = info;
-		if (rdna4_device_init(info, sSharedInfo[index]) != B_OK)
+		status_t status = rdna4_device_init(info, sSharedInfo[index]);
+		if (status != B_OK)
 			continue;
 
 		char name[64];
@@ -62,6 +79,7 @@ find_devices()
 			B_KERNEL_READ_AREA | B_KERNEL_WRITE_AREA);
 		if (sSharedAreas[index] < 0)
 			return sSharedAreas[index];
+
 		memcpy(address, &sSharedInfo[index], sizeof(rdna4_shared_info));
 		sDeviceCount++;
 	}
@@ -75,16 +93,18 @@ init_hardware(void)
 {
 	if (get_module(B_PCI_MODULE_NAME, (module_info**)&sPCI) != B_OK)
 		return B_ERROR;
-	status_t status = find_devices();
-	for (int32 i = 0; i < sDeviceCount; i++) {
-		if (sSharedAreas[i] >= 0)
-			delete_area(sSharedAreas[i]);
-		rdna4_device_uninit(sSharedInfo[i]);
-		free(sDeviceNames[i]);
-		sDeviceNames[i] = NULL;
-		sSharedAreas[i] = -1;
+
+	status_t status = B_ENTRY_NOT_FOUND;
+	int32 cookie = 0;
+	pci_info info;
+	while (sPCI->get_nth_pci_info(cookie++, &info) == B_OK) {
+		if (info.vendor_id == RDNA4_VENDOR_ID
+			&& info.class_base == PCI_display && info.class_sub == PCI_vga
+			&& rdna4_lookup_device(info.device_id) != NULL) {
+			status = B_OK;
+			break;
+		}
 	}
-	sDeviceCount = 0;
 	put_module(B_PCI_MODULE_NAME);
 	return status;
 }
@@ -92,29 +112,26 @@ init_hardware(void)
 extern "C" status_t
 init_driver(void)
 {
+	for (int32 i = 0; i < RDNA4_MAX_CARDS; i++)
+		sSharedAreas[i] = -1;
+
 	status_t status = get_module(B_PCI_MODULE_NAME, (module_info**)&sPCI);
 	if (status != B_OK)
 		return status;
+
 	status = find_devices();
 	if (status != B_OK) {
+		reset_devices();
 		put_module(B_PCI_MODULE_NAME);
-		return status;
+		sPCI = NULL;
 	}
-	return B_OK;
+	return status;
 }
 
 extern "C" void
 uninit_driver(void)
 {
-	for (int32 i = 0; i < sDeviceCount; i++) {
-		if (sSharedAreas[i] >= 0)
-			delete_area(sSharedAreas[i]);
-		rdna4_device_uninit(sSharedInfo[i]);
-		free(sDeviceNames[i]);
-		sDeviceNames[i] = NULL;
-		sSharedAreas[i] = -1;
-	}
-	sDeviceCount = 0;
+	reset_devices();
 	if (sPCI != NULL)
 		put_module(B_PCI_MODULE_NAME);
 	sPCI = NULL;
@@ -129,6 +146,8 @@ publish_devices(void)
 static int32
 find_index(const char* name)
 {
+	if (name == NULL)
+		return -1;
 	for (int32 i = 0; i < sDeviceCount; i++)
 		if (strcmp(name, sDeviceNames[i]) == 0)
 			return i;
@@ -170,7 +189,7 @@ static status_t
 device_ioctl(void* cookie, uint32 op, void* buffer, size_t length)
 {
 	int32 index = (int32)(addr_t)cookie;
-	if (index < 0 || index >= sDeviceCount)
+	if (index < 0 || index >= sDeviceCount || buffer == NULL)
 		return B_BAD_VALUE;
 
 	if (op == B_GET_ACCELERANT_SIGNATURE) {
@@ -183,14 +202,17 @@ device_ioctl(void* cookie, uint32 op, void* buffer, size_t length)
 	if (op == RDNA4_GET_PRIVATE_DATA) {
 		if (length < sizeof(rdna4_get_private_data))
 			return B_BUFFER_OVERFLOW;
+
 		rdna4_get_private_data data;
 		status_t status = user_memcpy(&data, buffer, sizeof(data));
 		if (status != B_OK || data.magic != RDNA4_PRIVATE_DATA_MAGIC)
 			return B_BAD_VALUE;
+
 		data.shared_info_area = clone_area("amdgpu_rdna4_shared", NULL,
 			B_ANY_ADDRESS, B_READ_AREA | B_WRITE_AREA, sSharedAreas[index]);
 		if (data.shared_info_area < 0)
 			return data.shared_info_area;
+
 		return user_memcpy(buffer, &data, sizeof(data));
 	}
 

cfd3bc1a6a6b36b994c5bd2db74cd4c9f55fffa1
kenmays <maybird1776@yahoo.com>
2026-09-15 05:56:57 -0700

    amdgpu_rdna4: add GFX12 VM mapping state

--- src/add-ons/kernel/drivers/graphics/amdgpu_rdna4/rdna4_vm.h
+++ src/add-ons/kernel/drivers/graphics/amdgpu_rdna4/rdna4_vm.h
@@ -3,16 +3,31 @@
 
 #include <SupportDefs.h>
 
+/* GFX12 uses 48-bit virtual addresses, 4 KiB pages and four page-table
+ * levels.  This class owns the software representation; hardware table
+ * programming is enabled by the kernel driver once its MMIO context is
+ * available. */
 class RDNA4VM {
 public:
 	RDNA4VM();
+	~RDNA4VM();
+
 	status_t Initialize();
 	status_t Map(uint64 gpuVA, uint64 physical, uint64 size, uint64 flags);
 	status_t Unmap(uint64 gpuVA, uint64 size);
 	status_t Flush();
 
 private:
+	struct Mapping {
+		uint64 gpuVA;
+		uint64 physical;
+		uint64 size;
+		uint64 flags;
+		Mapping* next;
+	};
+
 	bool fInitialized;
+	Mapping* fMappings;
 };
 
 #endif

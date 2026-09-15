53d2913c44a3500937cc061766e890899070b0ac
kenmays <maybird1776@yahoo.com>
2026-09-15 05:31:11 -0700

    graphics: add RDNA4 SDMA interface

---
+++ src/add-ons/kernel/drivers/graphics/amdgpu_rdna4/rdna4_sdma.h
@@ -0,0 +1,17 @@
+#ifndef RDNA4_SDMA_H
+#define RDNA4_SDMA_H
+
+#include <SupportDefs.h>
+
+class RDNA4SDMA {
+public:
+	RDNA4SDMA();
+	status_t Initialize();
+	status_t Copy(uint64 dst, uint64 src, uint64 size, uint64* _fence);
+	status_t Fill(uint64 dst, uint64 size, uint32 value, uint64* _fence);
+
+private:
+	bool fInitialized;
+};
+
+#endif

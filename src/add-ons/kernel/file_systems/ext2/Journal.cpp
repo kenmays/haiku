88145602972616d1de26e2afeb4301af4f600d65
kenmays <maybird1776@yahoo.com>
2026-09-19 09:46:40 -0700

    jbd2: initialize journal UUID storage

--- src/add-ons/kernel/file_systems/ext2/Journal.cpp
+++ src/add-ons/kernel/file_systems/ext2/Journal.cpp
@@ -109,6 +109,7 @@ Journal::Journal(Volume* fsVolume, Volume* jVolume)
 	fFeature64bits(false),
 	fChecksumSeed(0)
 {
+	memset(fJournalUUID, 0, sizeof(fJournalUUID));
 	recursive_lock_init(&fLock, "ext2 journal");
 	mutex_init(&fLogEntriesLock, "ext2 journal log entries");
 

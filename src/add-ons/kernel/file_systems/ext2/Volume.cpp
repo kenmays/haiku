ddb912e8a4f2a4e76e4168349338bd4a2c64537a
kenmays <maybird1776@yahoo.com>
2026-09-19 07:50:22 -0700

    ext4: accept supported 32/64-byte group descriptors

--- src/add-ons/kernel/file_systems/ext2/Volume.cpp
+++ src/add-ons/kernel/file_systems/ext2/Volume.cpp
@@ -218,9 +218,8 @@ Volume::Mount(const char* deviceName, uint32 flags)
 	if (Has64bitFeature()) {
 		TRACE("64bits\n");
 		fGroupDescriptorSize = fSuperBlock.GroupDescriptorSize();
-		if (fGroupDescriptorSize < sizeof(ext2_block_group))
-			return B_ERROR;
-		if (fGroupDescriptorSize != sizeof(ext2_block_group))
+		if (fGroupDescriptorSize < EXT2_BLOCK_GROUP_NORMAL_SIZE
+			|| fGroupDescriptorSize > sizeof(ext2_block_group))
 			return B_UNSUPPORTED;
 	} else
 		fGroupDescriptorSize = EXT2_BLOCK_GROUP_NORMAL_SIZE;
